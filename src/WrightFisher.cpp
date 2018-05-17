ff2e96c68b57015a5bc491c864adf5d73c05e538
Ivan Krukov <ivan.o.kryukov@gmail.com>
2018-05-16 22:00:00 -0600

    fixed out-of-bounds bug

--- src/WrightFisher.cpp
+++ src/WrightFisher.cpp
@@ -306,7 +306,7 @@ WrightFisher::Matrix WrightFisher::Switching(const lvec& N, const absorption_typ
                     else {
                         if (r.start == 0) {
                             W.Q.append_data(r.Q, m_start, m_end - 1, 1, r_last, row_complete);
-                            W.R(row - 1, j) = r.Q(0);
+                            W.R(im + offset - 1, j) = r.Q(0);
                         } else {
                             W.Q.append_data(r.Q, m_start - 1, m_end - 1, 0, r_last, row_complete);
                         }
@@ -318,7 +318,7 @@ WrightFisher::Matrix WrightFisher::Switching(const lvec& N, const absorption_typ
                     else {
                         if (r.end == N(j) * 2) {
                             W.Q.append_data(r.Q, m_start, m_end - 1, 0, r_last - 1, row_complete);
-                            W.R(row, j) = r.Q(r_last);
+                            W.R(im + offset, j) = r.Q(r_last);
                         } else {
                             W.Q.append_data(r.Q, m_start, m_end, 0, r_last, row_complete);
                         }
@@ -330,14 +330,14 @@ WrightFisher::Matrix WrightFisher::Switching(const lvec& N, const absorption_typ
                     else {
                         if (r.start == 0 && r.end == N(j) * 2) {
                             W.Q.append_data(r.Q, m_start, m_end - 2, 1, r_last - 1, row_complete);
-                            W.R(row - 1, 2 * j) = r.Q(0);
-                            W.R(row - 1, (2 * j) + 1) = r.Q(r_last);
+                            W.R(im + offset - 1, 2 * j) = r.Q(0);
+                            W.R(im + offset - 1, (2 * j) + 1) = r.Q(r_last);
                         } else if (r.start == 0) {
                             W.Q.append_data(r.Q, m_start, m_end - 1, 1, r_last, row_complete);
-                            W.R(row - 1, 2 * j) = r.Q(0);
+                            W.R(im + offset - 1, 2 * j) = r.Q(0);
                         } else if (r.end == N(j) * 2) {
                             W.Q.append_data(r.Q, m_start - 1, m_end - 2, 0, r_last - 1, row_complete);
-                            W.R(row - 1, (2 * j) + 1) = r.Q(r_last);
+                            W.R(im + offset - 1, (2 * j) + 1) = r.Q(r_last);
                         } else {
                             W.Q.append_data(r.Q, m_start - 1, m_end - 1, 0, r_last, row_complete);
                         }
