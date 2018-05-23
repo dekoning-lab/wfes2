f53ed5718736bf75ed2565a41dc3c65b369ed5ae
Ivan Krukov <ivan.o.kryukov@gmail.com>
2018-05-23 17:45:47 -0600

    more tests still

--- src/test_exe.cpp
+++ src/test_exe.cpp
@@ -19,6 +19,7 @@ TEST_CASE("Binomial row", "[binom-row]") {
 	REQUIRE(approx_eq(a, b));
 }
 
+// Non-absorbing 
 TEST_CASE("Non-Absorbing WF - square", "[non-absorbing]") {
 	WF::Matrix W1 = WF::SingleAlt(100, 100, WF::NON_ABSORBING, 0, 0.5, 1e-9, 1e-9, 0);
 	dmat W2 = WFE::Single(100, 100, WF::NON_ABSORBING, 0, 0.5, 1e-9, 1e-9).first;
@@ -37,6 +38,7 @@ TEST_CASE("Non-Absorbing WF - narrow", "[non-absorbing]") {
 	REQUIRE(approx_eq(W1.Q.dense(), W2));
 }
 
+// Both absorbing
 TEST_CASE("Both-Absorbing WF - square", "[both-absorbing]") {
 	WF::Matrix W1 = WF::SingleAlt(100, 100, WF::BOTH_ABSORBING, 0, 0.5, 1e-9, 1e-9, 0);
 	dmat W2 = WFE::Single(100, 100, WF::BOTH_ABSORBING, 0, 0.5, 1e-9, 1e-9).first;
@@ -52,5 +54,45 @@ TEST_CASE("Both-Absorbing WF - wide", "[both-absorbing]") {
 TEST_CASE("Both-Absorbing WF - narrow", "[both-absorbing]") {
 	WF::Matrix W1 = WF::SingleAlt(20, 100, WF::BOTH_ABSORBING, 0, 0.5, 1e-9, 1e-9, 0);
 	dmat W2 = WFE::Single(20, 100, WF::BOTH_ABSORBING, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+// Fixation
+
+TEST_CASE("Fixation WF - square", "[fixation]") {
+	WF::Matrix W1 = WF::SingleAlt(100, 100, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(100, 100, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+TEST_CASE("Fixation WF - wide", "[fixation]") {
+	WF::Matrix W1 = WF::SingleAlt(100, 20, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(100, 20, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+TEST_CASE("Fixation WF - narrow", "[fixation]") {
+	WF::Matrix W1 = WF::SingleAlt(20, 100, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(20, 100, WF::FIXATION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+//Extinction
+
+TEST_CASE("Extinction WF - square", "[extinction]") {
+	WF::Matrix W1 = WF::SingleAlt(100, 100, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(100, 100, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+TEST_CASE("Extinction WF - wide", "[extinction]") {
+	WF::Matrix W1 = WF::SingleAlt(100, 20, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(100, 20, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
+	REQUIRE(approx_eq(W1.Q.dense(), W2));
+}
+
+TEST_CASE("Extinction WF - narrow", "[extinction]") {
+	WF::Matrix W1 = WF::SingleAlt(20, 100, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9, 0);
+	dmat W2 = WFE::Single(20, 100, WF::EXTINCTION_ONLY, 0, 0.5, 1e-9, 1e-9).first;
 	REQUIRE(approx_eq(W1.Q.dense(), W2));
 }
