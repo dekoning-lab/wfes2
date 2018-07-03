eac6f9c41defd97005d79e6674266bd3f60e32c1
Ivan Krukov <ivan.o.kryukov@gmail.com>
2018-07-03 14:30:12 -0600

    dont append to output by default

--- include/util.hpp
+++ include/util.hpp
@@ -5,8 +5,8 @@
 const static Eigen::IOFormat CSVFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", "\n");
 const static Eigen::IOFormat CSVRowFormat(Eigen::FullPrecision, Eigen::DontAlignCols, ", ", ", ");
 
-void write_matrix_to_file(const dmat& A, std::string path, bool append = true);
-void write_vector_to_file(const dvec& A, std::string path, bool append = true);
+void write_matrix_to_file(const dmat& A, std::string path, bool append = false);
+void write_vector_to_file(const dvec& A, std::string path, bool append = false);
 
 void print_vector(const dvec& src, const char* prefix = "", const char* postfix = "", const char* delim = ",");
 void print_vector(const lvec& src, const char* prefix = "", const char* postfix = "", const char* delim = ",");
